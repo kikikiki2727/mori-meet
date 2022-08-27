c1f7a0e832ed7b8caee675b3118587f9df07e9f6
kikikiki2727 <kimori27271212@gmail.com>
2022-08-27 17:28:10 +0900

    test

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup>
+const aaa = ref({});
+const bbb = ref("");
+onMounted(() => {
+  aaa.value = { a: "a", b: "b" };
+  console.log("onMounted", aaa.value);
+  bbb.value = "bbb";
+  console.log("onMounted", bbb.value);
+});
+</script>
